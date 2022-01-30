dcc2ed2604afce3cddb23f921a51600076265a35
KowsarRahman <s3825159@student.rmit.edu.au>
2022-01-30 17:51:27 +1100

    statements feature started

---
+++ McbaExampleWithLogin/Controllers/MyStatementsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using McbaExample.Data;
+using McbaExample.Models;
+using McbaExample.Utilities;
+using McbaExampleWithLogin.Filters;
+
+namespace McbaExampleWithLogin.Controllers
+{
+    public class MyStatementsController : Controller
+    {
+        private readonly McbaContext _context;
+        private int CustomerID => HttpContext.Session.GetInt32(nameof(Customer.CustomerID)).Value;
+
+        public MyStatementsController(McbaContext context) => _context = context;
+
+
+
+        
+
+
+    }
+}
